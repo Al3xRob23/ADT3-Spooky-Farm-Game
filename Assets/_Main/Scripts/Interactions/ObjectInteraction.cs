0c7f3597fb815d7b2f3e1543d0d07ed9f93e8298
DJMiniMerx <146781793+DJMiniMerx@users.noreply.github.com>
2024-07-25 15:54:11 +1200

    Added Object interaction script

---
+++ Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+
+
+ void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)
+        {
+        }
+    }
