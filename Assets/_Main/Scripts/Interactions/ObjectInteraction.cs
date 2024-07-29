213def2aba6386a0203189fa4507f798a1a05445
DJMiniMerx <146781793+DJMiniMerx@users.noreply.github.com>
2024-07-30 09:11:37 +1200

    Revert "fixed compilation errors"
    This reverts commit 49c59d83703d640df99fcd475ac7e8b4f940dd07.

--- Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
+++ Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectInteraction : MonoBehaviour { }
+public class ObjectInteraction : MonoBehaviour
 
 
-
-  
+ void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E)
+        {
+        }
+    }
