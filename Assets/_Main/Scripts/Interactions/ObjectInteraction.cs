49c59d83703d640df99fcd475ac7e8b4f940dd07
DJMiniMerx <146781793+DJMiniMerx@users.noreply.github.com>
2024-07-30 08:57:46 +1200

    fixed compilation errors

--- Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
+++ Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectInteraction : MonoBehaviour
+public class ObjectInteraction : MonoBehaviour { }
 
 
- void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E)
-        {
-        }
-    }
+
+  
