7e2ccb46ff6a3267c177dbcf8aca86a86a45b8bf
DJMiniMerx <146781793+DJMiniMerx@users.noreply.github.com>
2024-07-30 09:13:10 +1200

    Fixed Map

--- Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
+++ Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
@@ -2,12 +2,4 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectInteraction : MonoBehaviour
-
-
- void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E)
-        {
-        }
-    }
+public class ObjectInteraction : MonoBehaviour { }
