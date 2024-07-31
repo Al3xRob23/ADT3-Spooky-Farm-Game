9f92b34a99573c99812fb13689a2a581eda9ce63
Alex Robinson <robiam3@student.op.ac.nz>
2024-08-01 11:46:22 +1200

    Got shader pack to work

--- Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
+++ Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
@@ -18,13 +18,8 @@ public class ObjectInteraction : MonoBehaviour
                 if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
                 {
                     Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                    Debug.Log("Did Hit");
                     Debug.Log("Interacted with: " + hit.collider.gameObject.name);
-                }else
-        {
-            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
-            Debug.DrawRay(transform.position, forward, Color.green);
-        }
+                }
                 
             }   
         }
