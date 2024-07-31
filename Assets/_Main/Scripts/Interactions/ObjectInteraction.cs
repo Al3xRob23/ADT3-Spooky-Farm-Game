9fcd52071b18021db823c2af96000d0dd761e875
Alex Robinson <robiam3@student.op.ac.nz>
2024-08-01 11:09:35 +1200

    Added raycast to interaction script
    Raycast outputs name of object hit when it collides with an object.

--- Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
+++ Assets/_Main/Scripts/Interactions/ObjectInteraction.cs
@@ -2,4 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectInteraction : MonoBehaviour { }
+public class ObjectInteraction : MonoBehaviour 
+    {   
+    
+    
+    
+        void Update() 
+        {
+            int layerMask = 1 << 5;
+            layerMask = ~layerMask;
+            if (Input.GetKey(KeyCode.E))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
+                {
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    Debug.Log("Did Hit");
+                    Debug.Log("Interacted with: " + hit.collider.gameObject.name);
+                }else
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * 10;
+            Debug.DrawRay(transform.position, forward, Color.green);
+        }
+                
+            }   
+        }
+    }
