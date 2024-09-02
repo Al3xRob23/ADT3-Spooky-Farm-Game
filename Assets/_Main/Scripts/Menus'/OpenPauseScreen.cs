418e7d5d18f25734f4ced8df0e41c06917c16367
Alex Robinson <robiam3@student.op.ac.nz>
2024-09-03 11:36:51 +1200

    Started on pause menu

---
+++ Assets/_Main/Scripts/Menus'/OpenPauseScreen.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenPauseScreen : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+    [SerializeField] CharacterController characterController;
+    public bool PauseMenuOpen;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.SetActive(!panel.activeInHierarchy);
+            if (panel.activeInHierarchy)
+            {
+                // We know that the game is paused.
+                Time.timeScale = 0.0f;
+                PauseMenuOpen = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                Cursor.visible = true;
+            }
+            else
+            {
+                // We know that the game has been resumed.
+                Time.timeScale = 1.0f;
+                PauseMenuOpen = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        PauseMenuOpen = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Quitting");
+    }
+}
