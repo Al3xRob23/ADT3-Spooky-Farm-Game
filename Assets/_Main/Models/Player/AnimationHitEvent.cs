c68f63a83c4aabaaf169a421ca133dc1088868ae
DJMiniMerx <146781793+DJMiniMerx@users.noreply.github.com>
2024-09-05 10:48:31 +1200

    Update AnimationHitEvent.cs

--- Assets/_Main/Models/Player/AnimationHitEvent.cs
+++ Assets/_Main/Models/Player/AnimationHitEvent.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimationHitEvent : MonoBehaviour
+
 {
     public void AE_Hit(string s) => Debug.Log("AE_Hit called at" + Time.time + " with a value of " + s);
+    
+
+
 }
 
