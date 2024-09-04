a90f5cd17f658cd38aad14cdd35467f6d5d919df
Alex Robinson <robiam3@student.op.ac.nz>
2024-09-05 11:26:35 +1200

    Aniim event now calls attack function

--- Assets/_Main/Models/Player/AnimationHitEvent.cs
+++ Assets/_Main/Models/Player/AnimationHitEvent.cs
@@ -1,9 +1,17 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationHitEvent : MonoBehaviour
 {
-    public void AE_Hit(string s) => Debug.Log("AE_Hit called at" + Time.time + " with a value of " + s);
+    public GameObject parent;
+    //public void AE_Hit(string s) => Debug.Log("AE_Hit called at" + Time.time + " with a value of " + s);
+
+    public void AE_Hit()
+    {
+        parent.GetComponent<SimpleFPSController>().EnemyHit();
+    }
 }
 
