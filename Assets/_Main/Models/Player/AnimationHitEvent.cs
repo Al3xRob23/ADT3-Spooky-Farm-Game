7bd06dfded69a4833ccb39c8d0e323e5deb39faa
DJMiniMerx <146781793+DJMiniMerx@users.noreply.github.com>
2024-10-15 12:33:27 +1300

    Fixed character rotating when attacking

--- Assets/_Main/Models/Player/AnimationHitEvent.cs
+++ Assets/_Main/Models/Player/AnimationHitEvent.cs
@@ -5,13 +5,23 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnimationHitEvent : MonoBehaviour
+
 {
     public GameObject parent;
     //public void AE_Hit(string s) => Debug.Log("AE_Hit called at" + Time.time + " with a value of " + s);
+    public Animator Animator;
 
     public void AE_Hit()
     {
         parent.GetComponent<SimpleFPSController>().EnemyHit();
+        Invoke("SetObjRotation", 0.8f); 
+    }
+
+    public void SetObjRotation()
+    {
+        
+        Animator.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        
     }
 }
 
